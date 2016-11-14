64f430b5a52162edecc0063009f2ddcc108b6cf7
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-11-14 06:37:51 +0100

    v0.0.1

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import {msg} from "./test";
+
+console.log("hello", msg);
+
+// TODO 01 basics
+
+// TODO 02 classes
+
+// TODO 04 generics
---
+++ app/test.ts
@@ -0,0 +1,3 @@
+const msg: string = "ts-for-ng2-tasks";
+
+export {msg};
